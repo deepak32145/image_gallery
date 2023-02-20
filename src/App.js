26724f91def3a046d1fd2b5fa7db1a2295701f73
deepak choudhary <deepak32145@gmail.com>
2023-02-20 22:46:13 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,38 @@ import GetSpecificimage from "./GetSpecificimage";
 export default function App() {
   return (
     <>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-pink-500 mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a
+              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+              href="#pablo"
+            >
+              Image gallery
+            </a>
+          </div>
+          <div id="example-navbar-danger">
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="/"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="/search"
+                >
+                  Search
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<GetImages />}></Route>
